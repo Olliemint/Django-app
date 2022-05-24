0616c288cd118e0d6b89324afd4b03a1166d011c
Olliemint <furywrld5@gmail.com>
2022-05-24 15:59:22 +0300

    ...

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.welcome,name='Welcome'),
-    path('today/',views.news_of_day,name='NewsToday'),
+    
+    path('',views.news_of_day,name='NewsToday'),
     path('archives/<past_date>/',views.past_days_news,name = 'pastNews') 
 ]
--- news/views.py
+++ news/views.py
@@ -2,12 +2,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
-def welcome(request):
-    
-    
-    return render(request,'index.html',)
+
 
 
 
@@ -18,8 +16,17 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    news = Article.todays_news()
+    return render(request,'all-news/today-news.html',{"date": date,"news": news})
 
-    return render(request,'all-news/today-news.html',{"date": date,})
+
+def convert_dates(dates):
+     # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
 
 
 # View Function to present news from past days
@@ -36,5 +43,6 @@ def past_days_news(request, past_date):
 
     if date == dt.date.today():
         return redirect(news_of_day)
+    news = Article.days_news(date)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    return render(request, 'all-news/past-news.html', {"date": date,"news": news})
