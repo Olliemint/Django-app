e796368d310e36535c24fb259d5abc973cbad66b
Olliemint <furywrld5@gmail.com>
2022-05-25 13:42:08 +0300

    ...

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path,re_path
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     
     path('',views.news_of_day,name='NewsToday'),
-    path('archives/<past_date>/',views.past_days_news,name = 'pastNews') 
+    path('archives/<past_date>/',views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results'),
+    re_path(r'article/(\d+)',views.article,name ='article')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tribune/settings.py
+++ tribune/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -132,3 +133,8 @@ STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
